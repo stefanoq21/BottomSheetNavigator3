0cd0e61b43ca0e7d669344481d9c06bc1f8f7a5a
DESKTOP-P3D71NI\Stefano <stefanoq21@hotmail.it>
2024-09-19 13:14:12 +0200

    just a refinement

--- app/src/main/java/com/stefanoq21/bottomsheetnavigator3/MainActivity.kt
+++ app/src/main/java/com/stefanoq21/bottomsheetnavigator3/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
