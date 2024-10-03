f5475b40a181327f53aa7f28bf1f336a0ad83a0b
DESKTOP-P3D71NI\Stefano <stefanoq21@hotmail.it>
2024-10-03 11:45:38 +0200

    minSdk version to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.stefanoq21.bottomsheetnavigator3"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 35
         versionCode = 10
         versionName = "0.0.7"
--- material3-navigation/build.gradle.kts
+++ material3-navigation/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 26
+        minSdk = 21
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
